68e4ab89b4efff5c8eaf5c0c07ccd0998878684d
张万豪 <18373049@buaa.edu.cn>
2020-11-10 00:25:40 +0800

    创建了两个新URL

--- web_for_dbwork2/urls.py
+++ web_for_dbwork2/urls.py
@@ -14,4 +14,7 @@ from web_for_dbwork2 import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='n_page2')
+
 ]
--- web_for_dbwork2/views.py
+++ web_for_dbwork2/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the  index.")
+
+def page1(request):
+    return HttpResponse("zwhnb!")
+
+def page2(request):
+    return HttpResponse("zwhnbnb!!!")
